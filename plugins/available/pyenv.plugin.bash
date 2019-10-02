3adc37b6dec611ff2fde2b88fca54c08f9e15a0e
plrectco <xy.ou@foxmail.com>
2019-10-02 14:57:17 -0700

    fix conflict

--- plugins/available/pyenv.plugin.bash
+++ plugins/available/pyenv.plugin.bash
@@ -4,11 +4,7 @@ about-plugin 'load pyenv, if you are using it'
 export PYENV_ROOT="$HOME/.pyenv"
 pathmunge "$PYENV_ROOT/bin"
 
-<<<<<<< HEAD
 [[ `which pyenv 2>/dev/null` ]] && eval "$(pyenv init - bash)"
-=======
-[[ `which pyenv` ]] && eval "$(pyenv init - bash)"
->>>>>>> update custom
 
 #Load pyenv virtualenv if the virtualenv plugin is installed.
 if pyenv virtualenv-init - &> /dev/null; then
